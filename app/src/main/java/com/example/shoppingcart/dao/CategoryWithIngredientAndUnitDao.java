cf4e7495139d7b3aa93be36e9c77b3ce148ce17c
itougou <itou@icc.core.ac.jp>
2022-12-30 21:00:59 +0900

    分類タブ画面改良、食材一覧画面改良

--- app/src/main/java/com/example/shoppingcart/dao/CategoryWithIngredientAndUnitDao.java
+++ app/src/main/java/com/example/shoppingcart/dao/CategoryWithIngredientAndUnitDao.java
@@ -3,10 +3,7 @@ package com.example.shoppingcart.dao;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Query;
-import androidx.room.Transaction;
 
-import com.example.shoppingcart.entity.CategoryWithIngredient;
-import com.example.shoppingcart.entity.CategoryWithIngredient2;
 import com.example.shoppingcart.entity.CategoryWithIngredientAndUnit;
 
 import java.util.List;
@@ -15,6 +12,6 @@ import java.util.List;
 public interface CategoryWithIngredientAndUnitDao {
     //全部のカテゴリーごとの食材情報を取り出す
     @Query("SELECT * FROM categoryWithIngredientAndUnit" )
-    public LiveData<List<CategoryWithIngredientAndUnit>> findAll();
+    LiveData<List<CategoryWithIngredientAndUnit>> findAll();
 
 }
